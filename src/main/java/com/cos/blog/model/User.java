6b402b8f594084b282fc23f51b51c5459d50f9dc
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-11-19 00:46:21 +0900

    id로 select 테스트 및 예외처리

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -21,7 +21,7 @@ public class User {
 
     @Id //Primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY)//프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
-    private Long id; // 시퀀스, auto_increment
+    private int id; // 시퀀스, auto_increment
 
     @Column(nullable = false, length = 30)
     private String username; //아이디
--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,14 +4,48 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.function.Supplier;
+
+//html 파일이 아니라 data를 리턴해주는 controller = Restcontroller
 @RestController
 public class DummyControllerTest {
 
     @Autowired // 의존성 주입(DI)
     private UserRepository userRepository;
+
+    //{id} 주소로 파라미터를 전달 받을 수 있음.
+    //http://localhost:8000/bolg/dummy/user/3
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+        //user/4을 찾으면 내가 내가 데이터베이스에서 찾지 못하면 user가 널이 될것이다.
+        //그렇게 되면 return이 null이 되므로 오류가 생길 수 있다.
+        //Optional로 너의 User객첼르 감싸서 가져올테니 null인지 아닌지 판단해서 return해라.
+
+//        //람다식
+//        User user = userRepository.findById(id).orElseThrow(()->{
+//            return new IllegalArgumentException("해당 사용자는 없습니다. id: "+id);
+//        });
+
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+            @Override
+            public IllegalArgumentException get() {
+                return new IllegalArgumentException("해당 사용자가 없습니다. id: "+id);
+            }
+        });
+
+        // 요청 : 웹브라우저
+        // user 객체 = 자바 오브젝트
+        //변환 (웹브라우저가 이해할 수 있는 데이터) -> json(Gson 라이브러리)
+        //스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+        //만약 자바 오브젝트를 리턴하게 되면 MessageConverter가 jackson라이버리를 호출해서
+        //user 오브젝트를 json으로 변환해서 브라우저에게 던져준다.
+        return user;
+    }
 
     //http://localhost:8000/blog/dummy/join (요청)
     //http의 body에 username, password, email 데이터를 가지고 (요청)
