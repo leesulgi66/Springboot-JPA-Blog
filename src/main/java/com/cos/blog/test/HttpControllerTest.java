bf113469f7e6c766b86604e8a4f3722b1e74a418
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-11-18 13:57:47 +0900

    http 요청 실습2

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -11,18 +11,18 @@ public class HttpControllerTest {
 
     //인터넷 브라우저 요청은 무조껀 get요청밖에 할 수 없다.
     @GetMapping("/http/get") //(select)
-    public String getTest() {
-        return "get 요청";
+    public String getTest(Member m) { //@RequestParam으로 각각 받지 않아도 스프링이 알아서 Member 객체에 쿼리스트링을 넣어준다! (누가 -> MessageConverter(스프링부트))
+        return "get 요청 : " +m.getId()+" ,"+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
     }
 
     @PostMapping("/http/post") //(insert)
-    public String postTest() {
-        return "post 요청";
+    public String postTest(@RequestBody Member m) {
+        return "post 요청 : " +m.getId()+" ,"+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
     }
 
     @PutMapping("/http/put") //(update)
-    public String putTest() {
-        return "put 요청";
+    public String putTest(@RequestBody Member m) {
+        return "put 요청 : "+m.getId()+" ,"+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
     }
 
     @DeleteMapping("/http/delete") //(delete)
