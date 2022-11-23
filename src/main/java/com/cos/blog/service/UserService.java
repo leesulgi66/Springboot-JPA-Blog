ea3c5d31ebf8d5b411f9ea9bfea6e4c173f94fa6
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-11-23 17:49:23 +0900

    주석 수정

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 //스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
-//Service를 쓰는 이유 1. 트랜젝션 관리, 2. 서비스의 의미(여러개의 CRUD가 묶여 하나의 Service가 되고 하나의 Transactional로 묶여 작동한다)
+//Transactional = 일이 처리되기 위한 가장 작은 단위
+//Service를 쓰는 이유 1. 트랜젝션 관리, 2. 서비스의 의미(여러개의 Transactional이 묶여 하나의 Service가 되고 하나의 Transactional로 묶여 작동한다)
 @Service
 public class UserService {
 
