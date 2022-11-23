7a33aa4674b7e6ef2bfe55e9a3c1338302b1cba9
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-11-23 15:52:13 +0900

    회원가입 예외처리 코드 수정(상태값, data)

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.cos.blog.handler;
 
+import com.cos.blog.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value=IllegalArgumentException.class) //IllegalArgumentException이 발생하면 아래 함수로 (Spring이)전달해 준다.
-    public String handleArgumentException(IllegalArgumentException e) {
-        return "<h1>"+e.getMessage()+"</h1>";
-    }
+//    @ExceptionHandler(value=IllegalArgumentException.class) //IllegalArgumentException이 발생하면 아래 함수로 (Spring이)전달해 준다.
+//    public String handleArgumentException(IllegalArgumentException e) {
+//        return "<h1>"+e.getMessage()+"</h1>";
+//    }
 
     @ExceptionHandler(value=Exception.class) //최고 부모인 Exception을 넣어주면 모든 예외가 이쪽으로 들어온다. 하지만 상세한 구분을 할 수 없다.
-    public String handleArgumentException(Exception e) {
-        return "<h1>"+e.getMessage()+"</h1>";
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage());
     }
 }
