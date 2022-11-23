5a8a3b4694d4a3ea2e1302466f2c4d7976bc9297
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-11-23 15:40:05 +0900

    회원가입 하기 완료

---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,29 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+//스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
+//Service를 쓰는 이유 1. 트랜젝션 관리, 2. 서비스의 의미(여러개의 CRUD가 묶여 하나의 Service가 되고 하나의 Transactional로 묶여 작동한다)
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public int 회원가입(User user) {
+        try {
+            userRepository.save(user);
+            return 1;
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("UserService : 회원가입() : " +e.getMessage());
+        }
+        return -1;
+    }
+}
