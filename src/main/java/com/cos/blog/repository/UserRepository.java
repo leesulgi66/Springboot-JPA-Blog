d8bbe2368b6467bffa7c8a33327e25eeef7a5b0f
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-11-24 19:20:13 +0900

    전통적인 방식의 로그인 방법

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.cos.blog.repository;
 
 import com.cos.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 // DAO
 // 자동으로 bean등록이 된다.
 //@Repository 생략이 가능하다
 public interface UserRepository extends JpaRepository<User, Integer> {
+    // JPA Naming 쿼리
+    // SELECT * FROM user WHERE =?1 AND password = ?2;
+    User findByUsernameAndPassword(String username, String password);
+
+    //위의 JPA 쿼리문을 nativeQuery로 사용하면 아래와 같음.
+    //@Query(value = "SELECT * FROM user WHERE =?1 AND password = ?2", nativeQuery = true)
+    //User login(String username, String password);
 }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -4,8 +4,8 @@ import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 //스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
 //Transactional = 일이 처리되기 위한 가장 작은 단위
@@ -19,5 +19,10 @@ public class UserService {
     @Transactional
     public void 회원가입(User user) {
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true) //Select할 때 트랜잭션 시작, 서비스 종료시에 트랜젝션 종료(정합성 유지)  *save가 없지만 transactional을 사용하는 이유
+    public User 로그인(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
     }
 }
