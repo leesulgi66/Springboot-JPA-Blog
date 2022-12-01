264d8d1d511459eb6a64eddbfdbf74a919eaf36d
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-01 19:39:59 +0900

    글 수정하기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -28,6 +28,12 @@ public class BoardController {
         return "board/detail";
     }
 
+    @GetMapping("/board/{id}/updateForm")
+    public String updateFrom(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/updateForm";
+    }
+
     @GetMapping({"/board/saveFrom"})
     public String saveFrom() {
         return "board/saveFrom";
