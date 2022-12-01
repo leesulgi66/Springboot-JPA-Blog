4c296e2f7dab7a367640a5a383fd0d344619ab72
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-01 14:35:05 +0900

    글목록 보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,15 +1,21 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"","/"})
-    public String index() {
-        // /WEB-INF/views/index.jsp
-        return "index";
+    public String index(Model model) {
+        model.addAttribute("boards", boardService.글목록());
+        return "index"; //viewResolver 작동!!
     }
 
     @GetMapping({"/board/saveFrom"})
