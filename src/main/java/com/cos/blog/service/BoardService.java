5171281c61ac5685f75cecb88cb20a147cc06d33
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-01 17:39:25 +0900

    글목록 페이징하기

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -4,10 +4,10 @@ import com.cos.blog.model.Board;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 
 @Service
@@ -23,7 +23,7 @@ public class BoardService {
         boardRepository.save(board);
     }
 
-    public List<Board> 글목록() {
-        return boardRepository.findAll();
+    public Page<Board> 글목록(Pageable pageable) {
+        return boardRepository.findAll(pageable);
     }
 }
