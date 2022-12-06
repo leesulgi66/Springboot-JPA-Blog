483777b646ebc8a13fe2ba4a9e162272a8f8a058
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-06 15:26:02 +0900

    회원수정1

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
         $("#btn-save").on("click", ()=>{ //function(){} 이 아니라 ()=>{}를 사용하는 이유는 this를 바인딩하기 위해서
             this.save();
         });
+        $("#btn-update").on("click", ()=>{ //function(){} 이 아니라 ()=>{}를 사용하는 이유는 this를 바인딩하기 위해서
+            this.update();
+        });
     },
 
     save: function() {
@@ -29,6 +32,32 @@ let index = {
                 alert("회원가입에 실패하였습니다.");
             }else{
                 alert("회원가입이 완료되었습니다.");
+                location.href = "/";
+            }
+
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+
+    update: function() {
+        let data = {
+            id: $("#id").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        };
+
+        $.ajax({
+            type: "PUT",
+            url: "/user",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(resp){
+            if(resp.status === 500){
+                alert("회원수정이 실패하였습니다.");
+            }else{
+                alert("회원수정이 완료되었습니다.");
                 location.href = "/";
             }
 
