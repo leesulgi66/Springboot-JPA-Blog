32f778824189c5d5ed6806828eedc54db9093ddc
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-06 18:03:06 +0900

    회원수정2 session 정보 변경

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -4,6 +4,7 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -46,6 +47,7 @@ public class UserService {
         String encPassword = encoder.encode(rawPassword);
         psersistance.setPassword(encPassword);
         psersistance.setEmail(user.getEmail());
+
         //회원수정 함수 종료시 = 서비스 종료 = 트랜잭셕 종료 = commit이 자동으로 됨.
         //영속화된 persistance 객체의 변화가 감지되면 더티체킹으로 update문을 날려줌.
     }
